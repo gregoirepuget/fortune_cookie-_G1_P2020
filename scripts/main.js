212c0eb7d8d4165f3c8ca3b49362f9eb916fac6a
gregoirepuget <gregoirepuget@gmail.com>
2016-03-15 10:46:55 +0100

    ajout champs auteur

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,14 @@ $(document).ready(function(){
   
   $("#numberField").attr("max",citations.length);
   
+  var options='';
+  for(var i=0; i<citations.length;i++ )
+  {
+    options += '<option value="'+i+'">'+citations[i]["auteur"]+'</option>';
+  }
+  $("#authorField").html(options);
+  
+  
   
  $("#fortune_button").on("click",function(e){
  
@@ -15,6 +23,18 @@ $(document).ready(function(){
    $("#fortune_cookie").append("<span>"+citations[nombre]["auteur"]+"</span>");
  
  });
+  
+$("#numberField, #authorField").on("change", function(){
+  
+    var nombre=$(this).val();
+  
+   $("#fortune_cookie").empty();
+   $("#fortune_cookie").append("<p>"+citations[nombre]["citation"]+"</p>");
+   $("#fortune_cookie").append("<span>"+citations[nombre]["auteur"]+"</span>");
+
+});
+  
+$("#cookieForm").on("submit", function(e){e.preventDefault();});
   
   
   function getTirage(limit)
