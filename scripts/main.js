892c5bdf7eaf5185014a5998ba414119f6bdb835
gregoirepuget <gregoirepuget@gmail.com>
2016-03-15 11:00:55 +0100

    ajout addCitation

--- scripts/main.js
+++ scripts/main.js
@@ -1,44 +1,46 @@
 $(document).ready(function(){
   console.log(citations);
-  
+
   $("#numberField").attr("max",citations.length);
-  
+
   var options='';
   for(var i=0; i<citations.length;i++ )
   {
     options += '<option value="'+i+'">'+citations[i]["auteur"]+'</option>';
   }
   $("#authorField").html(options);
-  
-  
-  
- $("#fortune_button").on("click",function(e){
- 
-   e.preventDefault();
-   
-   var nombre= getTirage(citations.length);
-   
-   $("#fortune_cookie").empty();
-   $("#fortune_cookie").append("<p>"+citations[nombre]["citation"]+"</p>");
-   $("#fortune_cookie").append("<span>"+citations[nombre]["auteur"]+"</span>");
- 
- });
-  
-$("#numberField, #authorField").on("change", function(){
-  
-    var nombre=$(this).val();
-  
-   $("#fortune_cookie").empty();
-   $("#fortune_cookie").append("<p>"+citations[nombre]["citation"]+"</p>");
-   $("#fortune_cookie").append("<span>"+citations[nombre]["auteur"]+"</span>");
 
-});
-  
-$("#cookieForm").on("submit", function(e){e.preventDefault();});
-  
-  
+
+
+  $("#fortune_button").on("click",function(e){
+
+    e.preventDefault();
+
+    var nombre= getTirage(citations.length);
+    addCitation(nombre);
+
+  });
+
+  $("#numberField, #authorField").on("change", function(){
+
+    var nombre=$(this).val();
+    addCitation(nombre);
+
+  });
+
+  $("#cookieForm").on("submit", function(e){e.preventDefault();});
+
+
   function getTirage(limit)
   {
     return Math.floor(Math.random()*limit);
+  }
+
+  function addCitation(numeroCitation)
+  {
+    $("#fortune_cookie").empty();
+    $("#fortune_cookie").append("<p>"+citations[numeroCitation]["citation"]+"</p>");
+    $("#fortune_cookie").append("<span>"+citations[numeroCitation]["auteur"]+"</span>");
+
   }
 });
